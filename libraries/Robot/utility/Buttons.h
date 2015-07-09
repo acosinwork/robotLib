7dbb50f314d542dfba166bbaa31e58b65420ebbe
acosinwork <acosinwork@yandex.ru>
2015-07-09 20:21:30 +0300

    click

--- libraries/Robot/utility/Buttons.h
+++ libraries/Robot/utility/Buttons.h
@@ -9,11 +9,29 @@
 
 class Buttons{
 public:
-  void check()
+
+  uint8_t getState()
+  {
+  	_check();
+  	return _currentFiltredState;
+  }
+
+
+private:
+
+  TimeOut _checkFQ;
+  uint8_t _buttonCheckCounter;
+  uint8_t _lastButtonState[MEASURE_COUNT];
+  uint8_t _currentFiltredState;
+  uint8_t _lastFilteredState;
+  uint8_t _newPress;
+  uint8_t _newRelease;
+
+  void _check()
   {
     if (_checkFQ.timeout(BUTTON_TIMEOUT_CHECK)) {
 
-    	uint8_t measureNum = ++_buttonCheckCounter % MEASURE_COUNT;
+      uint8_t measureNum = ++_buttonCheckCounter % MEASURE_COUNT;
         _lastButtonState[measureNum] = buttonsState();
 
         uint8_t pressed, released;
@@ -22,24 +40,18 @@ public:
 
         for (uint8_t i = 0; i < MEASURE_COUNT; ++i)
         {
-        	pressed &= _lastButtonState[i];
-        	released |= _lastButtonState[i];
+          pressed &= _lastButtonState[i];
+          released |= _lastButtonState[i];
         }
+        _lastFilteredState = _currentFiltredState;
         _currentFiltredState = pressed & released;
+
+        // something wrong (maybe bad):
+//TODO
+        // get all changes from 0 to 1
+        _newPress = (_lastFilteredState ^ _currentFiltredState) & (~_currentFiltredState);
+        //get all changes from 1 to 0
+        _newRelease = (_lastFilteredState ^ _currentFiltredState) & (~_lastFilteredState);
     }
   }
-
-  uint8_t getState()
-  {
-  	check();
-  	return _currentFiltredState;
-  }
-
-private:
-
-  TimeOut _checkFQ;
-  uint8_t _buttonCheckCounter;
-  uint8_t _lastButtonState[MEASURE_COUNT];
-  uint8_t _currentFiltredState;
-
 };
